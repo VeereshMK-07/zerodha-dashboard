914fba6a30bbd0e7386aced2f2c1010567f8f923
Veeresh Kakamari <veereshkakamri@gmail.com>
2026-03-21 23:38:41 +0530

    added toaster for logout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter , Routes, Route} from "react-router-dom";
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
 import Home from "./components/Home";
 
 const params = new URLSearchParams(window.location.search);
@@ -13,14 +15,14 @@ if (token) {
   window.history.replaceState({}, document.title, "/");
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route path="/*" element={<Home />}/>
-    </Routes>
+      <Routes>
+        <Route path="/*" element={<Home />} />
+      </Routes>
+      <ToastContainer position="top-right" autoClose={1500} theme="colored" />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
