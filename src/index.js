0623b7c4cbce2ccfe55df688b7b7f87741e362ad
Veeresh Kakamari <veereshkakamri@gmail.com>
2026-03-24 23:29:18 +0530

    fixed demo  issue

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
@@ -6,23 +6,32 @@ import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 import Home from "./components/Home";
 
-const params = new URLSearchParams(window.location.search);
-const token = params.get("token");
+const AppWrapper = () => {
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const token = params.get("token");
 
-if (token) {
-  localStorage.setItem("token", token);
-  // clean URL after saving token
-  window.history.replaceState({}, document.title, "/");
-}
+    if (token) {
+      localStorage.setItem("token", token);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
+      // clean URL
+      window.history.replaceState({}, document.title, "/");
+    }
+  }, []);
+
+  return (
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<Home />} />
       </Routes>
       <ToastContainer position="top-right" autoClose={1500} theme="colored" />
     </BrowserRouter>
-  </React.StrictMode>,
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <AppWrapper />
+  </React.StrictMode>
 );
